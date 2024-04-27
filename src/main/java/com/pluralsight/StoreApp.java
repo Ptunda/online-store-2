4b9e83099c8fced11fd77a902db4b5b7299c0853
Ptunda <ptunda@pgh.yearup.org>
2024-04-27 18:20:41 -0400

    created an inventory file

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -1,3 +1,10 @@
+
+// Develop an intuitive online store application capable of seamlessly integrating with your inventory data stored in a file named 'inventory.csv.'
+// Additionally, ensure the app facilitates effortless inventory updates by syncing with a file labeled 'newArrivals.csv.'
+// This functionality guarantees smooth operations, keeping your store up-to-date with the latest products and inventory levels
+
+// this is an African food store in Pittsburgh, PA
+
 package com.pluralsight;
 
 public class StoreApp {
