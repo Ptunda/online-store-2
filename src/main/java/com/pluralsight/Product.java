75b358b4c21f66081028a74bc5c3036f793ac500
Ptunda <ptunda@pgh.yearup.org>
2024-04-27 20:15:22 -0400

    Set up file reading functionality for 'inventory.csv'

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -2,5 +2,34 @@ package com.pluralsight;
 
 public class Product {
 
+    // private attributes
+    private int id;
+    private String description;
+    private double price;
+    private String department;
 
+    // constructor
+    public Product(int id, String description, double price, String department) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        this.department = department;
+    }
+
+    // getters of the private attributes
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }
--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -7,9 +7,83 @@
 
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class StoreApp {
 
+    static final String FILENAME = "inventory.csv";
+
     public static void main(String[] args) {
+
+        ArrayList<Product> inventory = loadInventory();
+
+        System.out.println("\nWelcome to our online store:\n");
+
+        int i = 1;
+
+        // loop to display items in the store
+        for(Product product : inventory){
+
+            System.out.println(i + ". ID: " + product.getId() + " - " + product.getDescription() + " - $" + product.getPrice() + " - " + product.getDepartment());
+            i++;
+
+        }
+
+    }
+
+    // method that creates the initial inventory
+    public static ArrayList<Product> loadInventory(){
+
+        // create the instance the loadInventory() method
+        ArrayList<Product> products = new ArrayList<>();
+
+        // create a file reading functionality for inventory.csv
+        try {
+
+            FileReader fileReader = new FileReader(FILENAME);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line; // a String to read each line from inventory.csv
+
+            while((line = bufferedReader.readLine()) != null){
+
+                // extract values from inventory.csv
+                String[] parts = line.split("\\|");
+
+                // validate that the array has the expected size
+                if (parts.length == 4){
+
+                    int id = Integer.parseInt(parts[0]);
+                    String description = parts[1];
+                    double price = Double.parseDouble(parts[2]);
+                    String department = parts[3];
+
+                    // create an instance of the Product class
+                    Product product = new Product(id, description, price, department);
+
+                    products.add(product); // add objects to the ArrayList
+
+                }
+
+            }
+
+            bufferedReader.close();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("FileNotFoundException occurred! " + e.getMessage());
+
+        } catch (IOException e){
+
+            System.out.println("IOException occurred! " + e.getMessage());
+
+        }
+
+        return products;
 
     }
 }
